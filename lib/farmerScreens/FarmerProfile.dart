39ae7271369f3923b620a629e510149dcd0c6d71
MadhuniYashodha <madhuniyashodha99@gmail.com>
2023-10-29 10:47:29 +0530

    add subscription page

--- lib/farmerScreens/FarmerProfile.dart
+++ lib/farmerScreens/FarmerProfile.dart
@@ -13,6 +13,7 @@ class FarmerProfile extends StatefulWidget {
 }
 
 class _FarmerProfileState extends State<FarmerProfile> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(

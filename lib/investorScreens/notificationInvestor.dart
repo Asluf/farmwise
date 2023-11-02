39b274c8647256534b79a06d8096b3ac5a370c0d
Nasik <132061754+MohamedNasik1998@users.noreply.github.com>
2023-11-02 15:40:46 +0530

    add notification page

--- lib/investorScreens/notificationInvestor.dart
+++ lib/investorScreens/notificationInvestor.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class NotificationInvestor extends StatefulWidget {
-  const NotificationInvestor({super.key});
+  const NotificationInvestor({Key? key}) : super(key: key);
 
   @override
   State<NotificationInvestor> createState() => _NotificationInvestorState();
@@ -10,8 +10,6 @@ class NotificationInvestor extends StatefulWidget {
 class _NotificationInvestorState extends State<NotificationInvestor> {
   @override
   Widget build(BuildContext context) {
-    final double wid = MediaQuery.of(context).size.width;
-    final double hei = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         flexibleSpace: Container(
@@ -22,19 +20,135 @@ class _NotificationInvestorState extends State<NotificationInvestor> {
             ),
           ),
         ),
-        title: Text("Notifications"),
+        title: const Text("Notifications"),
       ),
       backgroundColor: Colors.grey[200],
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.white, // White container
-              margin: EdgeInsets.all(10.0),
-              padding: EdgeInsets.all(10),
-              width: wid - 20,
-              child: Text("Notificationsssssssss"),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle Mark All Notifications button press
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 5, 46, 2),
+                  ),
+                  child: const Text('Mark All Notifications'),
+                ),
+              ],
             ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                bool isAccepted = index.isEven;
+                return GestureDetector(
+                  onTap: () {
+                    // Handle the tap on the NotificationCard
+                    // For example, print the tapped index
+                    print('Tapped index: $index');
+                  },
+                  child: NotificationCard(
+                    name: 'Ishani $index',
+                    isAccepted: isAccepted,
+                    date: 'Date',
+                    // Replace 'Date' with the actual date
+                    time: 'Time', // Replace 'Time' with the actual time
+                    onPressed: () {
+                      // Handle individual notification button press
+                      print('Button in notification $index pressed.');
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Handle View All Notifications button press
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 5, 46, 2),
+              ),
+              child: const Text('View All Notifications'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NotificationCard extends StatelessWidget {
+  final String name;
+  final bool isAccepted;
+  final String date;
+  final String time;
+  final VoidCallback onPressed;
+
+  const NotificationCard({
+    Key? key,
+    required this.name,
+    required this.isAccepted,
+    required this.date,
+    required this.time,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // margin: const EdgeInsets.symmetric(horizontal: 16.0),
+      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: <Widget>[
+            Icon(
+              Icons.notifications, // Use the notifications icon
+              size: 30,
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    name,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                    
+                      Text(
+                        'Description',
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Spacer(),
+                      Text(
+                        'Date: $date, Time: $time',
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            
           ],
         ),
       ),
